f5cf29ad0e17e568607b3de0d7528309ff32c70c
Yanis101K <yaniskaced41@gmail.com>
2025-10-10 10:40:47 +0100

    find first uniq character

---
+++ strings/10_first_uniq_character.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def firstUniqChar( s: str) -> int:
+        char_freq = Counter(s) 
+
+        for index , char in enumerate( s ) : 
+            if char_freq[char] == 1 : 
+                return  index 
+        
+
+        return -1 ; 
+
+# time complexity : O(N), N is the length of our string 
+# we need to go over all the charcters in our string in order to assign them to the Counter 
+# space complexity : O(K) , K is the number of uniq elements in the string 
+# each uniq element in the string will need one memeory space in the Counter  
